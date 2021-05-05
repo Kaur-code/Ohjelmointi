ae1e3ef864662159fb1169f6e334190621473a98
Kaur-code <79134890+Kaur-code@users.noreply.github.com>
2021-05-05 09:47:49 +0300

    Update Aakkosjärjestys.js

--- Koodaushaasteet/Aakkosjärjestys.js
+++ Koodaushaasteet/Aakkosjärjestys.js
@@ -1,7 +1,8 @@
 function aakkosJarjestys(x) {
-  let wow = Array.from(x);
-  let result = wow.sort();
+  let wow = Array.from(x); //'edcba' => 'e,d,c,b,a'
+  let pow = wow.sort(); //'e,d,c,b,a' => 'a,b,c,d,e'
+  let result = pow.join(''); //'a,b,c,d,e' => 'abcde'
   return result;
 };
 
-console.log(aakkosJarjestys('keajgijeaigpa'))
+console.log(aakkosJarjestys('edcba'))
