3d204f6f6301e723a55f84271cf4c5c0dd77bf8c
Kaur-code <79134890+Kaur-code@users.noreply.github.com>
2021-09-08 09:46:11 +0300

    Delete Muistipeli.js

--- Muistipeli.js
+++
@@ -1,8 +0,0 @@
-const jsKortti = document.querySelectorAll(".kortit");
-
-function kaannaKortti() {
-  console.log('poopy');
-  console.log(this);
-}
-
-jsKortti.forEach(kortti => kortti.addEventListener('click'. kaannaKortti))
